f7dfff3a04e3ff237b0a08b0647c7fc827a2b5ab
Roman Kolesnev <roman.kolesnev.v@gmail.com>
2024-06-09 21:02:59 +0200

    Nord theme for nvim

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,4 +1,36 @@
+--
+-- better defaults
+--
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+vim.opt.swapfile = false
+
+-- 
+-- lazy.nvim setup
+--
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+--
+-- Nord colorscheme
+--
+require("lazy").setup({
+  "nordtheme/vim",
+  lazy=false,
+  config = function ()
+    vim.cmd("colorscheme nord")
+  end
+})
