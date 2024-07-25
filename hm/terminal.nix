0a699d0c22cf7b4165a0716a6a62312e517df5a9
Roman Kolesnev <roman.kolesnev.v@gmail.com>
2024-07-26 00:37:15 +0200

    terminal: switch to Kitty as terminal+multiplexer
    + add yazi as a file manager

--- hm/terminal.nix
+++ hm/terminal.nix
@@ -1,8 +1,6 @@
 #
 # Terminal Home Manager Module
 #
-# It responsible for setting up a terminal app and a terminal
-# multiplexer.
 {
   pkgs,
   lib,
@@ -11,12 +9,9 @@
   ...
 }: let
   mkOutOfStoreSymlink = config.lib.file.mkOutOfStoreSymlink;
-  tmuxConfHomePath = ".config/tmux/tmux.conf";
-  alacrittyConfHomePath = ".config/alacritty/alacritty.toml";
 in {
   home.packages = with pkgs; [
-    alacritty
-    tmux
+    kitty # TODO: focus on kitty, use programs.kitty, fetchFromGithub alternative icon & nord theme
   ];
 
   home.file = {
@@ -24,23 +19,15 @@ in {
     # configuration files directly to the files in the dotfiles
     # directory of this repository. (Instead of making them part of the
     # Nix store.)
-    ${tmuxConfHomePath}.source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/dotfiles/tmux.conf";
-    ${alacrittyConfHomePath}.source = mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/dotfiles/alacritty.toml";
-
-    # It's not very reproducible but I want to use TPM for tmux
-    # plugins. Unfortunately many tmux plugins in Nixpkgs are
-    # significantly outdated. Also, I there is no TPM in Nixpkgs. But
-    # I can still use Nix to install TPM.
-    ".config/tmux/tpm".source = pkgs.fetchFromGitHub {
-      owner = "tmux-plugins";
-      repo = "tpm";
-      rev = "v3.1.0";
-      sha256 = "sha256-CeI9Wq6tHqV68woE11lIY4cLoNY8XWyXyMHTDmFKJKI=";
-    };
+    # TODO: add Kitty configuration
   };
 
   programs.zsh.shellAliases = {
-    tmux-config = "$EDITOR ~/${tmuxConfHomePath}";
-    alacritty-config = "$EDITOR ~/${alacrittyConfHomePath}";
+    # TODO: alias for kitty config?
+  };
+
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
   };
 }
