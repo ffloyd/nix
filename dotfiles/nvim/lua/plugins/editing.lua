d059dde3c26650b0304b2ed54a6574639794de85
Roman Kolesnev <roman.kolesnev.v@gmail.com>
2024-08-18 01:05:08 +0200

    nvim: better w,e,b motions

--- dotfiles/nvim/lua/plugins/editing.lua
+++ dotfiles/nvim/lua/plugins/editing.lua
@@ -27,5 +27,26 @@ return {
     -- this is equalent to setup({}) function
     -- 
     -- TODO: check for integrations in README, especially nvim-cmp
+  },
+  {
+    -- like subword-mode for EMACS
+    "chrisgrieser/nvim-spider",
+    keys = {
+      {
+        "w",
+        "<cmd>lua require('spider').motion('w')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "e",
+        "<cmd>lua require('spider').motion('e')<CR>",
+        mode = { "n", "o", "x" },
+      },
+      {
+        "b",
+        "<cmd>lua require('spider').motion('b')<CR>",
+        mode = { "n", "o", "x" },
+      }
+    }
   }
 }
