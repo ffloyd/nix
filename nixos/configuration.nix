a25c4188f44b12d2973285b963ef0b6008dce098
Roman Kolesnev <roman.kolesnev.v@gmail.com>
2024-12-19 18:51:25 +0100

    nixos: fix problems with wol.service

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -139,9 +139,16 @@
     description = "Enable Wake-on-LAN";
     after = ["network-online.target"];
     wants = ["network-online.target"];
+    script = ''
+      IFACE=$(ip addr | awk '/state UP/ {print $2}' | sed 's/.$//')
+
+      echo "Enabling Wake-on-LAN on $IFACE..."
+      ethtool -s $IFACE wol g
+      echo "Done"
+    '';
+    path = with pkgs; [iproute2 gawk gnused ethtool];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.ethtool}/bin/ethtool -s $(ip addr | awk '/state UP/ {print $2}' | sed 's/.$//') wol g";
       RemainAfterExit = true;
     };
     wantedBy = ["multi-user.target"];
