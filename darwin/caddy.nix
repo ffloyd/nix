859384431b5210dbbc1a964857f48b463f5b0300
Roman Kolesnev <roman.kolesnev.v@gmail.com>
2025-01-03 17:15:48 +0100

    osx: improve dnsmasq setup
    Integrated services.dnsmasq does not use wait-for-nix-store-path hack so
    it's unstable when loading. This commit fixes it by directly config
    dnsmasq launchd daemon.

--- darwin/caddy.nix
+++ darwin/caddy.nix
@@ -1,54 +1,85 @@
 {
   pkgs,
-  inputs,
+  lib,
   private,
   ...
 }: let
   caddy = pkgs.caddy;
-in {
-  services.dnsmasq = {
-    enable = true;
-    addresses = {
-      # I want all *.test domains to point to localhost
-      test = "127.0.0.1";
-      "lb.here" = "127.0.0.1";
-    };
+  dnsmasq = pkgs.dnsmasq;
+  addresses = {
+    test = "127.0.0.1";
+    "lb.here" = "127.0.0.1";
   };
-
-  environment.systemPackages = [caddy];
-
-  #
-  # I need a reverse HTTPS proxy for my projects to be able to access
-  # them via URLs like https://myproject.test
-  #
-  environment.etc."Caddyfile".text = ''
+  dnsmasqPort = 53;
+  dnsmasqBind = "127.0.0.1";
+  mapA = f: attrs: with builtins; attrValues (mapAttrs f attrs);
+  dnsmasqAddressesParamsList = mapA (name: address: "--address=/${name}/${address}") addresses;
+  dnsmasqAddressesParams = pkgs.lib.concatStringsSep " " dnsmasqAddressesParamsList;
+in
+  lib.mkMerge [
+    #
+    # I need a dnsmasq to resolve my custom domains to localhost
+    #
     {
-      local_certs
+      environment.systemPackages = [dnsmasq];
+
+      environment.etc = with builtins;
+        listToAttrs (map (domain: {
+          name = "resolver/${domain}";
+          value = {
+            enable = true;
+            text = ''
+              port ${toString dnsmasqPort}
+              nameserver ${dnsmasqBind}
+            '';
+          };
+        }) (builtins.attrNames addresses));
+
+      launchd.daemons.dnsmasq = {
+        command = "${dnsmasq}/bin/dnsmasq --keep-in-foreground --port=${toString dnsmasqPort} --listen-address=${dnsmasqBind} " + dnsmasqAddressesParams;
+        serviceConfig = {
+          KeepAlive = true;
+          RunAtLoad = true;
+        };
+      };
     }
 
-    https://lb.here {
-      reverse_proxy localhost:8080
+    #
+    # I need a reverse HTTPS proxy for my projects to be able to access
+    # them via URLs like https://myproject.test
+    #
+    {
+      environment.systemPackages = [caddy];
+
+      environment.etc."Caddyfile".text = ''
+        {
+          local_certs
+        }
+
+        https://lb.here {
+          reverse_proxy localhost:8080
+        }
+
+        ${private.workProjectsCaddyfile}
+      '';
+
+      launchd.daemons.caddy = {
+        command = "${caddy}/bin/caddy run --config /etc/Caddyfile";
+
+        environment = {
+          HOME = "/var/caddy";
+        };
+
+        serviceConfig = {
+          KeepAlive = true;
+
+          StandardErrorPath = "/var/caddy/stderr";
+          StandardOutPath = "/var/caddy/stdout";
+        };
+      };
+
+      system.activationScripts.extraActivation.text = ''
+        ${caddy}/bin/caddy trust || echo "Cannot add certificates. Please run 'caddy trust' manually."
+      '';
     }
-
-    ${private.workProjectsCaddyfile}
-  '';
-
-  launchd.daemons.caddy = {
-    command = "${caddy}/bin/caddy run --config /etc/Caddyfile";
-
-    environment = {
-      HOME = "/var/caddy";
-    };
-
-    serviceConfig = {
-      KeepAlive = true;
-
-      StandardErrorPath = "/var/caddy/stderr";
-      StandardOutPath = "/var/caddy/stdout";
-    };
-  };
-
-  system.activationScripts.extraActivation.text = ''
-    ${caddy}/bin/caddy trust || echo "Cannot add certificates. Please run 'caddy trust' manually."
-  '';
-}
+  ]
