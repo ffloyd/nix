ff28093562d944d5365fb3ace299c5f3fde27505
Roman Kolesnev <roman.kolesnev.v@gmail.com>
2025-11-26 22:53:49 +0100

    chore: update dependencies (opencode v1.0.115, openspec 0.16.0)

--- hm/development-environment/ai-tooling.nix
+++ hm/development-environment/ai-tooling.nix
@@ -123,7 +123,7 @@
     (let
       openspec = pkgs.buildNpmPackage rec {
         pname = "openspec";
-        version = "0.14.0";
+        version = "0.16.0";
 
         src = pkgs.fetchFromGitHub {
           owner = "Fission-AI";
