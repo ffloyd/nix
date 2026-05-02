77a3ab869d3c39b3fc3175da1ab1f18de1d77566
Roman Kolesnev <roman@ffloyd.space>
2026-05-02 16:00:17 +0200

    fix: adjust opencode.nvim setup

--- dotfiles/nvim/lua/ai.lua
+++ dotfiles/nvim/lua/ai.lua
@@ -5,13 +5,11 @@
 local features = require("features")
 
 features.add({
-  "AI assistants integration",
+  "OpenCode integration",
   after = { "which-key", "snacks" },
   plugins = {
     {
-      -- "nickjvandyke/opencode.nvim",
-      "ffloyd/opencode.nvim",
-      branch = "fix-for-nix",
+      "nickjvandyke/opencode.nvim",
       version = "*",
     }
   },
@@ -21,10 +19,15 @@ features.add({
 
     local opencode_cmd = "opencode --port"
 
+    local snacks_term = require("snacks.terminal")
+    local opencode = require("opencode")
+
     ---@type snacks.terminal.Opts
-    local snacks_terminal_opts = {
+    local opencode_terminal_opts = {
       win = {
         position = 'right',
+        -- By default we do not want to focus on terminal each time it's shown.
+        -- This should be an opt-in behaviour.
         enter = false,
         on_win = function(win)
           -- Set up keymaps and cleanup for an arbitrary terminal
@@ -37,44 +40,104 @@ features.add({
     vim.g.opencode_opts = {
       server = {
         start = function()
-          require('snacks.terminal').open(opencode_cmd, snacks_terminal_opts)
+          snacks_term.open(opencode_cmd, opencode_terminal_opts)
         end,
         stop = function()
-          require('snacks.terminal').get(opencode_cmd, snacks_terminal_opts):close()
+          snacks_term.get(opencode_cmd, opencode_terminal_opts):close()
         end,
         toggle = function()
-          require('snacks.terminal').toggle(opencode_cmd, snacks_terminal_opts)
+          snacks_term.toggle(opencode_cmd, opencode_terminal_opts)
         end,
       },
     }
 
+    local function get_opencode_term()
+      local opts = vim.tbl_deep_extend('force', opencode_terminal_opts, { create = false })
+      return snacks_term.get(opencode_cmd, opts)
+    end
+
+    -- Toggle OpenCode with focus on its window
+    local function toggle_with_focus()
+      opencode.toggle() -- this will start terminal
+
+      local term = get_opencode_term()
+
+      -- valid is false when terminal has no window
+      if term:valid() then
+        term:focus()
+      end
+    end
+
+    -- Shows OpenCode if hidded, then "asks" without submit
+    local function ask_with_term()
+      local term = get_opencode_term()
+
+      if not term or not term:valid() then
+        opencode.toggle()
+      end
+
+      opencode.ask("@this ")
+    end
+
     require("which-key").add({
       {
         "<c-.>",
-        function() require("opencode").toggle() end,
-        desc = "OpenCode Toggle",
+        toggle_with_focus,
+        desc = "Toggle OpenCode (with focus)",
         mode = { "n", "t", "i", "x" },
       },
-      { "<leader>aa", group = "AI OpenCode", mode = { "n", "x" } },
+      {
+        "<c-,>",
+        ask_with_term,
+        desc = "Ask OpenCode (ensures UI is visible)",
+        mode = { "n", "t", "i", "x" },
+      },
+      { "<leader>aa", group = "OpenCode (AI)", mode = { "n", "x" } },
       {
         "<leader>aaa",
-        function() require("opencode").ask("@this: ", { submit = true }) end,
-        desc = "Ask OpenCode",
+        function() opencode.ask("", { submit = true }) end,
+        desc = "Ask",
       },
       {
-        "<leader>aas",
-        function() require("opencode").select() end,
-        desc = "Select OpenCode Action",
+        "<leader>aaA",
+        function() opencode.ask() end,
+        desc = "Ask (no submit)",
       },
       {
-        "<leader>aaS",
-        function() require("opencode").select_server() end,
-        desc = "Select OpenCode Action",
+        "<leader>aac",
+        function() opencode.command("agent.cycle") end,
+        desc = "Cycle Agent",
       },
       {
-        "<leader>aat",
-        function() require("opencode").toggle() end,
-        desc = "Toggle OpenCode",
+        "<leader>aaC",
+        function() opencode.command("session.compact") end,
+        desc = "Compact Session",
+      },
+      {
+        "<leader>aa<Space>",
+        function() opencode.select() end,
+        desc = "Action Picker",
+      },
+      {
+        "<leader>aa<CR>",
+        function() opencode.command('prompt.submit') end,
+        desc = "Submit Current Prompt"
+      },
+      {
+        "go",
+        ---@diagnostic disable-next-line: redundant-return-value
+        function() return opencode.operator("@this ") end,
+        desc = "Send range to OpenCode",
+        mode = { "n", "x" },
+        expr = true,
+      },
+      {
+        "goo",
+        ---@diagnostic disable-next-line: redundant-return-value
+        function() return opencode.operator("@this ") .. "_" end,
+        desc = "Send line to OpenCode",
+        mode = "n",
+        expr = true,
       },
     })
   end
